940266f19f15c2bc825b57d4c1a93c6fd91cd75d
Hyperdimensionals <45752766+Hyperdimensionals@users.noreply.github.com>
2020-05-22 15:42:53 -0700

    Example app displaying types of QClickEdit widgets

---
+++ Examples.py
@@ -0,0 +1,40 @@
+import sys
+
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtCore import QTime
+
+import QClickEdit
+
+
+class Examples(QWidget):
+    def __init__(self, parent=None):
+        super(Examples, self).__init__(parent)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.temp = QClickEdit.QSpinBox(23, "Temperature", "C")
+
+        self.ontime = QClickEdit.QTimeEdit(QTime(1, 0, 0, 0), "On Time")
+        self.ontime.setDisplayFormat("h:mm:ss ap")
+
+        items = ['asdf', 23, 4, False, list]
+        self.boxxx = QClickEdit.QComboBox(items)
+        self.boxxx.addItem('aa')
+        self.boxxx.setValue('aa')
+
+        self.layout.addWidget(QClickEdit.QLineEdit("Blorka", "Name", "Sr."))
+        self.layout.addWidget(self.temp)
+        self.layout.addWidget(self.ontime)
+        self.layout.addWidget(self.boxxx)
+
+
+if __name__ == '__main__':
+    from PySide2.QtWidgets import QVBoxLayout
+
+    app = QApplication(sys.argv)
+
+    examples = Examples()
+    examples.show()
+
+    # Run main Loop
+    sys.exit(app.exec_())
